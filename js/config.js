3366d30099a0a41fc7b50556ee94b8428142f766
Venkatesan <ramajeevithan.venkat@accenture.com>
2025-07-29 17:07:22 -0500

    fix

--- js/config.js
+++ js/config.js
@@ -1,14 +1,14 @@
 
-// Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBq8KP7sman50oZhNuUl1GUtz6nSLaByQs",
-  authDomain: "secret-project-first.firebaseapp.com",
-  databaseURL: "https://secret-project-first-default-rtdb.firebaseio.com",
-  projectId: "secret-project-first",
-  storageBucket: "secret-project-first.firebasestorage.app",
-  messagingSenderId: "116932368381",
-  appId: "1:116932368381:web:d812ba62c9800b8cb0ab94",
-  measurementId: "G-L4HRFSD034"
+  apiKey: "AIzaSyBsaS3lw7sw2wZ2V8UyIpEoVL7Yt-_7ytI",
+  authDomain: "oviyasbabyshower.firebaseapp.com",
+  projectId: "oviyasbabyshower",
+  storageBucket: "oviyasbabyshower.firebasestorage.app",
+  databaseURL: "https://oviyasbabyshower-default-rtdb.firebaseio.com",
+  messagingSenderId: "325125072862",
+  appId: "1:325125072862:web:0ca394030298f13f379f4f",
+  measurementId: "G-VM3TKXQ1VZ"
 };
 
 // Initialize Firebase
